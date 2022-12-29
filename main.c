46fe241b55b09faa9e2e62f63f1cd55c18b007a8
Modzie <110020963+M0dzie@users.noreply.github.com>
2022-12-29 15:54:58 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,127 +6,78 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 17:16:26 by thmeyer           #+#    #+#             */
-/*   Updated: 2022/12/29 14:20:47 by thmeyer          ###   ########.fr       */
+/*   Updated: 2022/12/29 15:44:28 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-void	stack_clear(t_stack **stack)
-{
-	t_stack	*stack_next;
-
-	if (!stack)
-		return ;
-	while ((*stack))
-	{
-		stack_next = (*stack)->next;
-		stack_delone(*stack);
-		(*stack) = stack_next;
-	}
-	(*stack) = 0;
-}
-
-void	stack_delone(t_stack *stack)
-{
-	if (!stack)
-		return ;
-	stack->value = 0;
-	stack->next = NULL;
-	free(stack);
-}
-
-t_stack	*stack_new(int arg)
-{
-	t_stack	*new;
-
-	new = malloc(sizeof(t_stack));
-	if (!new)
-		exit(0);
-	if (new)
-	{
-		new->value = arg;
-		new->next = 0;
-	}
-	return (new);
-}
-
-t_stack	*stack_last(t_stack *stack)
-{
-	t_stack	*last_stack;
-
-	if (!stack)
-		return (0);
-	while (stack)
-	{
-		last_stack = stack;
-		stack = stack->next;
-	}
-	return (last_stack);
-}
-
-void	stack_add_back(t_stack **stack, t_stack *new)
-{
-	t_stack	*last_stack;
-
-	if (!(*stack))
-		(*stack) = new;
-	else
-	{
-		last_stack = stack_last((*stack));
-		last_stack->next = new;
-	}
-}
-
-void	print_stack(t_stack *stack)
+void	print_stack(t_list *stack)
 {
 	int		i;
-	t_stack	*tmp;
+	t_list	*tmp;
 
 	tmp = stack;
 	i = 0;
 	while (tmp)
 	{
-		printf("stack %ie : %i\n", i, tmp->value);
+		printf("stack %ie : %s\n", i, tmp->content);
 		i++;
 		tmp = tmp->next;
 	}
 }
 
-int	main(int argc, char **argv)
+char	**parsing_arg(char **argv)
 {
 	int		i;
-	int		j;
 	char	*arg;
 	char	**all_arg;
-	t_stack	*stack_a;
-	t_stack	*stack_b;
 
 	i = 0;
-	j = -1;
-	stack_a = NULL;
-	stack_b = NULL;
 	arg = malloc(sizeof(char));
 	if (!arg)
 		return (0);
-	if (argc <= 1)
-		return (0);
-	if (argc > 1)
+	while (argv[++i])
 	{
-		while (argv[++i])
+		arg = ft_strjoin(arg, argv[i]);
+		if (!arg)
 		{
-			arg = ft_strjoin(arg, argv[i]);
-			if (!arg)
-				return (free(arg), 0);
+			free(arg);
+			exit(0);
 		}
 	}
 	printf("argument(s) en string : %s\n", arg);
 	all_arg = ft_split(arg, ' ');
+	if (!all_arg)
+	{
+		free(arg);
+		exit(0);
+	}
 	for (int h = 0; all_arg[h]; h++)
 		printf ("%ie argument : %s\n", h, all_arg[h]);
+	return (all_arg);
+}
+
+
+
+int	main(int argc, char **argv)
+{
+	int		j;
+	char	**all_arg;
+	t_list	*stack_a;
+	t_list	*stack_b;
+
+	j = -1;
+	stack_a = NULL;
+	stack_b = NULL;
+	if (argc <= 1)
+		return (ft_putstr_fd("Error", 2), 0);
+	all_arg = parsing_arg(argv);
 	while (all_arg[++j])
-		stack_add_back(&stack_a, stack_new(ft_atoi(all_arg[j])));
+		ft_lstadd_back(&stack_a, ft_lstnew(all_arg[j]));
+	if (!stack_a)
+		return (free(all_arg), 0);
 	print_stack(stack_a);
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,39 +6,24 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 12:36:10 by thmeyer           #+#    #+#             */
-/*   Updated: 2022/12/29 14:33:52 by thmeyer          ###   ########.fr       */
+/*   Updated: 2022/12/29 15:39:19 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-# include <unistd.h>
-# include <stdlib.h>
 # include "Libft/libft.h"
 
-typedef struct s_stack
-{
-	int				value;
-	struct s_stack	*next;
-}					t_stack;
-
-char	double_reverse_rotate(t_stack *stack_a, t_stack stack_b);
-char	double_rotate(t_stack *stack_a, t_stack *stack_b);
-char	double_swap(t_stack *stack_a, t_stack *stack_b);
-char	push_a(t_stack *stack_a, t_stack *stack_b);
-char	push_b(t_stack *stack_b, t_stack *stack_a);
-char	reverse_rotate_a(t_stack *stack_a);
-char	reverse_rotate_b(t_stack *stack_b);
-char	rotate_a(t_stack *stack_a);
-char	rotate_b(t_stack *stack_b);
-char	swap_a(t_stack *stack_a);
-char	swap_b(t_stack *stack_b);
-
-t_stack	*stack_last(t_stack *stack);
-t_stack	*stack_new(int arg);
-
-void	stack_add_back(t_stack **stack, t_stack *new);
-void	stack_clear(t_stack **stack);
-void	stack_delone(t_stack *stack);
+char	double_reverse_rotate(t_list *stack_a, t_list stack_b);
+char	double_rotate(t_list *stack_a, t_list *stack_b);
+char	double_swap(t_list *stack_a, t_list *stack_b);
+char	push_a(t_list *stack_a, t_list *stack_b);
+char	push_b(t_list *stack_b, t_list *stack_a);
+char	reverse_rotate_a(t_list *stack_a);
+char	reverse_rotate_b(t_list *stack_b);
+char	rotate_a(t_list *stack_a);
+char	rotate_b(t_list *stack_b);
+char	swap_a(t_list *stack_a);
+char	swap_b(t_list *stack_b);
 
 #endif
