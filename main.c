2913943447470cf5bf2b4a61f9877a708c201635
Modzie <110020963+M0dzie@users.noreply.github.com>
2022-12-30 09:59:05 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,54 +6,12 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 17:16:26 by thmeyer           #+#    #+#             */
-/*   Updated: 2022/12/30 09:28:14 by thmeyer          ###   ########.fr       */
+/*   Updated: 2022/12/30 09:58:46 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
-
-int	ps_isdigit(char *str)
-{
-	int	i;
-
-	i = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	while (str[i] >= '0' && str[i] <= '9')
-		i++;
-	if (!str[i])
-		return (1);
-	return (0);
-}
-
-int	check_error(char **all_arg)
-{
-	int				i;
-	int				j;
-	long long int	int_arg;
-
-	i = 0;
-	while (all_arg[i])
-	{
-		if (!ps_isdigit(all_arg[i]))
-			return (1);
-		int_arg = ft_atoi(all_arg[i]);
-		if (int_arg > INT_MAX || int_arg < INT_MIN)
-			return (1);
-		j = i + 1;
-		while (all_arg[j])
-		{
-			if (int_arg == ft_atoi(all_arg[j]))
-				return (1);
-			j++;
-		}
-		i++;
-	}
-	return (0);
-}
 
 void	print_stack(t_list *stack)
 {
@@ -70,33 +28,10 @@ void	print_stack(t_list *stack)
 	}
 }
 
-char	**parsing_arg(char **argv)
-{
-	int		i;
-	char	*arg;
-	char	**all_arg;
-
-	i = 0;
-	arg = malloc(sizeof(char));
-	if (!arg)
-		return (0);
-	while (argv[++i])
-	{
-		arg = ft_strjoin(arg, argv[i]);
-		if (!arg)
-			exit(0);
-	}
-	printf("argument(s) en string : %s\n", arg);
-	all_arg = ft_split(arg, ' ');
-	if (!all_arg || check_error(all_arg) != 0)
-	{
-		ft_putstr_fd("Error", 2);
-		exit(0);
-	}
-	for (int h = 0; all_arg[h]; h++)
-		printf ("%de argument : %s\n", h, all_arg[h]);
-	return (all_arg);
-}
+// int	stack_is_sort(t_list *stack)
+// {
+// 	return (0);
+// }
 
 int	main(int argc, char **argv)
 {
@@ -109,13 +44,15 @@ int	main(int argc, char **argv)
 	stack_a = NULL;
 	stack_b = NULL;
 	if (argc <= 1)
-		return (ft_putstr_fd("Error", 2), 0);
+		return (ft_putstr_fd("Error\n", 2), 0);
 	all_arg = parsing_arg(argv);
 	while (all_arg[++j])
 		ft_lstadd_back(&stack_a, ft_lstnew(all_arg[j]));
 	if (!stack_a)
 		return (free(all_arg), 0);
 	print_stack(stack_a);
+	// if (!stack_is_sort(stack_a))
+	// 	sort(stack_a, stack_b);
 	return (0);
 }
 
@@ -124,5 +61,3 @@ int	main(int argc, char **argv)
 
 // Tout mettre sur la pile B, et check le plus haut et le plus bas de la pile
 // puis le mettre sur A : apparemment c'est le moins d'instructions
-
-// Faire une fonction check pour les doublons et int max int min via la str
